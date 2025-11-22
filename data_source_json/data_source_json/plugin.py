c571f014986e51d0f1098c0075f7efe7e493d484
Ana <kwon.ana244@gmail.com>
2025-11-22 17:31:19 +0100

    #9 Implementirana rekurzivna logika za konverziju JSON-a u graf

--- data_source_json/data_source_json/plugin.py
+++ data_source_json/data_source_json/plugin.py
@@ -2,7 +2,7 @@ import os
 import json
 from typing import Dict, Any, List
 from api.api.data_source import DataSourcePlugin, PluginParameter, PluginParameterType
-from api.api.graph import Graph
+from api.api.graph import Graph, Node, Edge
 
 class JSONDataSource(DataSourcePlugin):
 
@@ -26,6 +26,54 @@ class JSONDataSource(DataSourcePlugin):
         with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
 
-        graph = Graph(directed=True)
+        graph = Graph()
+        self.parsed_nodes = {}  #za pracenje vec kreiranih cvorova
+        
+        # root -> id pocetnog cvora
+        self.parse_recursive(graph, data, "root")
         
         return graph
+
+    def parse_recursive(self, graph: Graph, data: Any, current_id: str) -> Node:
+        # Provera ID-a unutar podataka (ako postoji @id polje, ono ima prednost)
+        if isinstance(data, dict) and "@id" in data:
+            current_id = data["@id"]
+        
+        #ako je vec kreiran cvor sa ovim ID-jem, vracamo ga
+        if current_id in self.parsed_nodes:
+            return self.parsed_nodes[current_id]
+
+        node = Node(id=current_id)
+        graph.add_node(node)
+        self.parsed_nodes[current_id] = node
+
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key == "@id": continue
+
+                if isinstance(value, (dict, list)):
+                    self.handle_complex_value(graph, node, key, value)
+                else:
+                    node.data[key] = value
+
+        elif isinstance(data, list):
+            for i, item in enumerate(data):
+                self.handle_complex_value(graph, node, f"item_{i}", item)
+
+        return node
+
+    def handle_complex_value(self, graph: Graph, source_node: Node, relation_name: str, value: Any):
+        # pomocna funkcija za kreiranje grane
+        def create_edge(target):
+            edge = Edge(source=source_node.id, target=target.id, data={"relation": relation_name})
+            graph.add_edge(edge)
+
+        if isinstance(value, list):
+            for i, item in enumerate(value):
+                child_id = item.get("@id") if isinstance(item, dict) else f"{source_node.id}_{relation_name}_{i}"
+                target_node = self.parse_recursive(graph, item, child_id)
+                create_edge(target_node)
+        else:
+            child_id = value.get("@id") if isinstance(value, dict) else f"{source_node.id}_{relation_name}"
+            target_node = self.parse_recursive(graph, value, child_id)
+            create_edge(target_node)
