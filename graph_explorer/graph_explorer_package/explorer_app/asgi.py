cc2a38ad22389422f28d0c33936cb691d8c0e389
Andro <andrija.slovic1@gmail.com>
2025-11-23 14:21:51 +0100

    #27 Refaktorisana struktura Django projekta graph_explorer radi bolje organizacije i instalacije lokalnih paketa

--- graph_explorer/graph_explorer_package/explorer_app/asgi.py
+++ graph_explorer/graph_explorer_package/explorer_app/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graph_explorer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "explorer_app.settings")
 
 application = get_asgi_application()
--- graph_explorer/graph_explorer_package/explorer_app/wsgi.py
+++ graph_explorer/graph_explorer_package/explorer_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graph_explorer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "explorer_app.settings")
 
 application = get_wsgi_application()
