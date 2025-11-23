89338bbfb4f1999be411dbb0ea5c419759ac42b5
Ana <kwon.ana244@gmail.com>
2025-11-23 20:32:26 +0100

    #13 Implementiran inicijalni UI za izbor Data Source plugina i konfiguracija Django putanja

--- graph_explorer/graph_explorer_package/manage.py
+++ graph_explorer/graph_explorer_package/manage.py
@@ -2,10 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from pathlib import Path
 
 def main():
     """Run administrative tasks."""
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent
+    sys.path.append(str(BASE_DIR))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "explorer_app.settings")
     try:
         from django.core.management import execute_from_command_line
