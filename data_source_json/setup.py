6c524805447be9c75b853177f8893f8947bbb2c2
Andro <andrija.slovic1@gmail.com>
2025-11-23 14:33:15 +0100

    #29 Azuriran author u svim setup.py fajlovima

--- data_source_json/setup.py
+++ data_source_json/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="data_source_json",
     version="0.1.0",
-    author="andro02",
+    author="KVT",
     description="Data source plugin koji učitava JSON strukture i formira graf model",
     packages=find_packages(),
     install_requires=[
--- simple_visualizer/setup.py
+++ simple_visualizer/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="simple_visualizer",
     version="0.1.0",
-    author="andro02",
+    author="KVT",
     description="Jednostavan vizualizer grafa zasnovan na D3.js force-layoutu",
     packages=find_packages(),
     install_requires=[
