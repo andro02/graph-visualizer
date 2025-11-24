b86ce83b523a0914606f89a41742b9c9d8d3ba5e
Ana <kwon.ana244@gmail.com>
2025-11-24 19:07:12 +0100

    #12 Implementacija backend logike: Integracija GraphManager-a i kreiranje API endpointa

--- graph_explorer/graph_explorer_package/core/apps.py
+++ graph_explorer/graph_explorer_package/core/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self):
+        # importujemo i inicijalizujemo platformu kad se Django digne
+        from graph_platform.platform.graph_manager import GraphManager
+        GraphManager() # inicijalizacija managera
